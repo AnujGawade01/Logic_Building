2bb00fd8bb8bb257df97c5a0aca6105faeac8f7a
AnujGawade01 <121755207+AnujGawade01@users.noreply.github.com>
2026-06-01 23:45:35 +0530

    Add files via upload

---
+++ program4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int i, j, k;
+
+    printf("Enter first number : \n");
+    scanf("%d",&i);
+
+    printf("Enter second number : \n");
+    scanf("%d",&j);
+
+    k = i + j;
+
+    printf("Addition is : %d\n",k);
+
+    return 0;
+}
+
+
+
+// which type of input will you use
+// how much accuracy is required
+//value will be positive or negative
