e3aaf4743c6311c10145ba9437901bedeb25d362
Anuj Vinayak Gawade <121755207+AnujGawade01@users.noreply.github.com>
2026-07-02 13:22:14 +0530

    Add files via upload

---
+++ program125.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int SumEven(int Arr[], int iSize)
+{
+    int iCnt = 0;
+    int iSum = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt]%2 == 0)
+        {
+            iSum = iSum + Arr[iCnt];
+        }
+    }
+
+    return iSum;
+}
+
+int main()
+{
+    int iLength = 0, iCnt = 0, iRet = 0;
+    int *Brr = NULL;
+
+    printf("Enter the size of array / number of elements : \n");
+    scanf("%d", &iLength);
+
+    Brr = (int *) malloc(sizeof(int) * iLength);
+
+    printf("Enter the elements : \n");
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d", &Brr[iCnt]);
+    }
+
+    iRet = SumEven(Brr, iLength);
+
+    printf("Even elements summation : %d\n", iRet);
+
+    free(Brr);
+
+    return 0;
+}
