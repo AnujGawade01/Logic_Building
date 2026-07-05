279bc3300e245e41ba10f91c8e28d21af88dff92
Anuj Vinayak Gawade <121755207+AnujGawade01@users.noreply.github.com>
2026-07-05 23:04:40 +0530

    Add files via upload

---
+++ program139.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class program139
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int Arr[] = {10,20,30,40,50};
+
+        for(int iCnt = 0; iCnt < Arr.length; iCnt++)        // works but not the right way
+        {
+            System.out.println(Arr[iCnt]);
+        }
+    }
+}
