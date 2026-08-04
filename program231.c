bdfe027f5901de6fc2fc0f296da8edd66210943c
Anuj Vinayak Gawade <121755207+AnujGawade01@users.noreply.github.com>
2026-08-04 14:43:04 +0530

    Add files via upload

---
+++ program231.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+    char str[] = {'J','a','y',' ','G','a','n','e','s','h',' '}; // It can print an unwanted character
+
+    printf("%s\n", str);
+    return 0;
+}
