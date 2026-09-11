0bde019cc364fb2094cc391001415c2fec8995ca
Anuj Vinayak Gawade <121755207+AnujGawade01@users.noreply.github.com>
2026-09-11 05:30:26 +0530

    Add files via upload

---
+++ program257.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int CountSpace(const char * str) 
+{
+    int iCount = 0;
+
+    while(*str != '\0')
+    {
+        if((*str == ' '))
+        {
+            iCount++;
+        }
+        str++;
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter string : \n");
+    scanf("%[^'\n']s",Arr);        
+
+    iRet = CountSpace(Arr);
+
+    printf(" Frequency of Space character is : %d\n", iRet);
+
+    return 0;
+}
